0cb47f4b68c90a6b23d7375a07a379f0cbff3d34
Tres Seaver <tseaver@palladion.com>
2013-06-24 17:52:49 -0400

    Py3k-compatible re-raise.

--- repoze/tm/__init__.py
+++ repoze/tm/__init__.py
@@ -4,6 +4,32 @@ import transaction
 
 ekey = 'repoze.tm.active'
 
+PY3 = sys.version_info[0] >= 3
+
+if PY3: #pragma NO COVER Py3k
+    def reraise(tp, value, tb=None):
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
+else:   #pragma NO COVER Python2
+    def exec_(_code_, _globs_=None, _locs_=None):
+        """Execute code in a namespace."""
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None: #pragma NO COVER
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
+
+    exec_("""def reraise(tp, value, tb=None):
+    raise tp, value, tb
+""")
+    del exec_
+
+
 class TM:
     """ Transaction management WSGI middleware """
     def __init__(self, application, commit_veto=None):
@@ -29,7 +55,7 @@ class TM:
             try:
                 type_, value, tb = sys.exc_info()
                 self.abort()
-                raise type_, value, tb
+                reraise(type_, value, tb)
             finally:
                 del type_, value, tb
 
